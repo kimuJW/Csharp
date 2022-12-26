2cd555a9f1f7eeb8abacd8aa0e467a85d5cf6174
kimuJW <a01020532509@gmail.com>
2022-12-26 15:56:14 +0900

    codes

---
+++ 2022-12-27-first.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch1
+{
+    class Test
+    {
+
+        static void Main(string[] args)
+        {
+         string s = "123";
+         float f =321;
+         int a = int.Parse(s);
+         int b = (int)f;
+         int hap = 0;
+         hap = a + b;
+         Console.WriteLine(hap);          
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
