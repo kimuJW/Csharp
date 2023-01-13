53055458cc24676c1b075e46130c9978ee8837de
kimuJW <a01020532509@gmail.com>
2023-01-13 12:44:00 +0000

    codes

---
+++ 2023-01-25-first.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ch12_객체동적생성
+{
+    public partial class 객체동적생성 : Form
+    {
+        public 객체동적생성()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("버튼 클릭했어요");
+        }
+    }
+}
